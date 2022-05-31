a62b0c49692df0def45d4cde32359309cb247422
QuangPham27 <101493497+QuangPham27@users.noreply.github.com>
2022-05-31 15:06:15 +0700

    Add files via upload
    Week 2

---
+++ Week 2/OOP interface 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OOP_interface_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            obj.Display();
+            IFirst first = obj;
+            first.Print();
+            ISecond second = obj;
+            second.Print();
+            Console.ReadLine();
+        }
+    }
+    public interface IFirst
+    {
+        void Print();
+        void Display();
+    }
+    public interface ISecond
+    {
+        void Print();
+    }
+    public class MyClass : IFirst, ISecond
+    {
+        public void Display()
+        {
+            Console.WriteLine("Display Method");
+        }
+        void IFirst.Print()
+        {
+            Console.WriteLine("IFirst's Print Method");
+        }
+        void ISecond.Print()
+        {
+            Console.WriteLine("ISecond's Print Method");
+        }
+    }
+}
