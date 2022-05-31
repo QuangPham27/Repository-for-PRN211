55fd2cf400f1d56ae127daac7e28bbea7db4cbe2
QuangPham27 <101493497+QuangPham27@users.noreply.github.com>
2022-05-31 15:03:35 +0700

    Add files via upload
    Week 1

---
+++ Week 1/Processing Command Line/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Welcome to C#.NET Programming";
+            Console.WriteLine("{0}",msg);
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"Arg: {args[i]}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
