23501ac3187f1d590fee7910e93c9faed7d84152
QuangPham27 <101493497+QuangPham27@users.noreply.github.com>
2022-05-31 15:11:30 +0700

    Add files via upload

---
+++ Week 3/FactoriesMethod/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FactoriesMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Factory Method Pattern Demo.***\n");
+            List<AnimalFactory> animalFactoryList = new List<AnimalFactory>
+            {
+                new TigerFactory(),
+                new LionFactory()
+            };
+            foreach (var animalFactory in animalFactoryList)
+            {
+                animalFactory.CreateAnimal().AboutMe();
+
+            }
+            Console.ReadLine();
+        }
+    }
+    public interface IAnimal
+    {
+        void AboutMe();
+    }
+    public class Lion : IAnimal
+    {
+        public void AboutMe() => Console.WriteLine("This is a Lion");
+
+    }
+    public class Tiger : IAnimal
+    {
+        public void AboutMe() => Console.WriteLine("This is a Tiger");
+
+    }
+    public abstract class AnimalFactory
+    {
+        public abstract IAnimal CreateAnimal();
+    }
+    public class LionFactory : AnimalFactory
+    {
+        public override IAnimal CreateAnimal() => new Lion();
+    }
+    public class TigerFactory : AnimalFactory
+    {
+        public override IAnimal CreateAnimal() => new Tiger();
+    }
+}
---
+++ Week 3/PrototypePattern/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace PrototypePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Prototype Pattern Demo***\n");
+
+            Car Mustang = new Mustang("Mustang EcoBoost");
+            Car Bentley = new Bentley("Continental GT Mulliner");
+
+            Console.WriteLine($"Car is: {Mustang.modelName}, and it's base price is Rs. {Mustang.BasePrice} ");
+            Console.WriteLine($"Car is: {Bentley.modelName}, and it's base price is Rs. {Bentley.BasePrice} ");
+
+            Car Car;
+            Car = Mustang.Clone();
+            Car.OnRoadPrice = Car.BasePrice + Car.SetAdditionalPrice();
+            Console.WriteLine($"Car is: {Mustang.modelName}, and it's base price is Rs. {Mustang.BasePrice} ");
+
+            Car = Bentley.Clone();
+            Car.OnRoadPrice = Car.BasePrice + Car.SetAdditionalPrice();
+            Console.WriteLine($"Car is: {Bentley.modelName}, and it's base price is Rs. {Bentley.BasePrice} ");
+            Console.ReadLine();
+        }
+    }
+    public abstract class Car
+    {
+        protected int basePrice = 0, onRoadPrice = 0;
+        public string modelName { get; set; }
+        public int BasePrice
+        {
+            set => basePrice = value;
+            get => basePrice;
+        }
+        public int OnRoadPrice
+        {
+            set => onRoadPrice = value;
+            get => onRoadPrice;
+        }
+        public static int SetAdditionalPrice()
+        {
+            Random random = new Random();
+            int AdditionalPrice = random.Next(200_000, 500_000);
+            return AdditionalPrice;
+        }
+        public abstract Car Clone();
+    }
+    public class Mustang : Car
+    {
+        public Mustang(string model) => (modelName, basePrice) = (model, 200_000);
+        public override Car Clone() => this.MemberwiseClone() as Mustang;
+    }
+    public class Bentley : Car
+    {
+        public Bentley(string model) => (modelName, BasePrice) = (model, 300_000);
+        public override Car Clone() => this.MemberwiseClone() as Bentley;
+    }
+}
