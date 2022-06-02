e6eb4a22f2f8b58348bc1e958e022a6f481fca4c
QuangPham27 <101493497+QuangPham27@users.noreply.github.com>
2022-06-02 13:42:02 +0700

    Slot 10

---
+++ Week 4/Slot 10/Parameterized Thread Start Delegate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace Parameterized_Thread_Start_Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ID of thread in Main(): {0}",
+                Thread.CurrentThread.ManagedThreadId);
+            MyParams p = new MyParams { value01 = 5, value02 = 15 };
+            Thread t = new Thread(new ParameterizedThreadStart(AddNumber));
+            t.IsBackground = true;
+            t.Start(p);
+            waitHandle.WaitOne();
+            Console.WriteLine("Main Thread: Done.");
+            Console.ReadLine();
+        }
+        static AutoResetEvent waitHandle = new AutoResetEvent(false);
+        static void AddNumber(object data)
+        {
+            if (data is MyParams p)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("ID of thread in Add(): {0}",
+                    Thread.CurrentThread.ManagedThreadId);
+                Console.WriteLine($"{p.value01} + {p.value02} = { p.value01 + p.value02}");
+                waitHandle.Set();
+            }
+        }
+    }
+    class MyParams
+    {
+        public int value01 { get; set; }
+        public int value02 { get; set; }
+    }
+
+}
---
+++ Week 4/Slot 10/Thread Pool/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Pool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Demo The CoreCLR Thread Pool *****");
+            Console.WriteLine("Main thread stated. ThreadID = {0}",
+                Thread.CurrentThread.ManagedThreadId);
+            Printer p = new Printer();
+            WaitCallback workItem = new WaitCallback(PrintTheNumbers);
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(workItem, p);
+            }
+            Console.WriteLine("All tasks queued.");
+            Console.ReadLine();
+        }
+        static void PrintTheNumbers(object state)
+        {
+            Printer task = (Printer)state;
+            task.PrintNumbers();
+        }
+    }
+    public class Printer
+    {
+        private object threadLock = new object();
+        public void PrintNumbers()
+        {
+            Monitor.Enter(threadLock);
+            try
+            {
+                Console.WriteLine("->{0} is executing PrintNumbers()", Thread.CurrentThread.ManagedThreadId);
+                for (int i = 1; i <= 5; i++)
+                {
+                    Random r = new Random();
+                    Thread.Sleep(500 * r.Next(5));
+                    Console.Write($"{i,3}{(i == 5 ? "" : ",")}");
+                }
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Monitor.Exit(threadLock);
+            }
+        }
+    }
+}
