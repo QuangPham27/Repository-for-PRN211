70052c8f133e44e85578cd27a2933565e4a3cf99
QuangPham27 <101493497+QuangPham27@users.noreply.github.com>
2022-06-06 20:07:52 +0700

    Slot 11

---
+++ Week 4/Slot 11/MySolution/MyConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static MyLibrary.MyClass;
+
+namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 50, b = 25;
+            int result;
+            Console.WriteLine("******Demo Consuming Assemblies******");
+            //Invoke Add Method
+            result = a.Add(b);
+            Console.WriteLine($"{a}+{b}={result}");
+            //Invoke Sub Method
+            result = a.Sub(b);
+            Console.WriteLine($"{a}-{b}={result}");
+            Console.ReadLine();
+
+        }
+    }
+}
