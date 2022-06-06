0fa0efb1e85ca2420d8d90df3bd6009e075e644c
QuangPham27 <101493497+QuangPham27@users.noreply.github.com>
2022-06-06 20:13:06 +0700

    Slot 12

---
+++ Week 4/Slot 12/FileInfo Class Demonstration/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace FileInfo_Class_Demonstration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"MyFile.txt";
+            Console.WriteLine("******Demo FileInfo Class");
+            //Create a text file
+            File.WriteAllText(fileName, "Hello World.");
+            //Read file content
+            Console.WriteLine("Read file:");
+            string content = File.ReadAllText(fileName);
+            Console.WriteLine(content);
+            Console.WriteLine("File information:");
+            //Get file information
+            FileInfo testFile = new FileInfo(fileName);
+            Console.WriteLine($"Name:{testFile.Name}");
+            //Creation time
+            Console.WriteLine($"Creation time: {testFile.CreationTime}");
+            //Last write time
+            Console.WriteLine($"Last Write Time: {testFile.LastWriteTime}");
+            //Name of parent directory
+            Console.WriteLine($"Directory Name: {testFile.DirectoryName}");
+            Console.ReadLine();
+        }
+    }
+}
