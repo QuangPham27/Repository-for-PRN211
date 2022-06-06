fbd40c49bca50e0fc4bb0db52b7968d5a91932c3
QuangPham27 <101493497+QuangPham27@users.noreply.github.com>
2022-06-06 20:14:47 +0700

    Slot 12

---
+++ Week 4/Slot 12/DirectoryInfo Class Demonstration/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryInfo_Class_Demonstration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo(@"C:\Users\admin\source\repos");
+            Console.WriteLine("Search pattern *a* returns:");
+            foreach (var fi in di.GetDirectories("*a*"))
+            {
+                Console.WriteLine(fi.Name);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Search pattern TopDirectoryOnly returns:");
+            foreach (var fi in di.GetFiles("*.txt",SearchOption.TopDirectoryOnly))
+            {
+                Console.WriteLine(fi.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week 4/Slot 12/File Class Demonstration 02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace File_Class_Demonstration_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"MyData.txt";
+            //This text is added only once to the file
+            if (!File.Exists(path))
+            {
+                //Create file to write to
+                string createText = "Hello and Welcome" + Environment.NewLine;
+                File.WriteAllText(path,createText);
+            }
+            string appendText = "This is extra" + Environment.NewLine;
+            File.AppendAllText(path, appendText);
+            //Open the file to read from
+            string readText = File.ReadAllText(path);
+            Console.WriteLine(readText);
+            Console.ReadLine();
+        }
+    }
+}
