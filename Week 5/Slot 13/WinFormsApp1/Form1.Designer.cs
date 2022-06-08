80e9eaf05dc272a6b2c9d7eac07a59b67328f061
QuangPham27 <101493497+QuangPham27@users.noreply.github.com>
2022-06-08 22:29:04 +0700

    Slot 13

---
+++ Week 5/Slot 13/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace WinFormsApp1
+{
+    partial class frmEmployeeDetails
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.lbEmpID = new System.Windows.Forms.Label();
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.txtEmployeeName = new System.Windows.Forms.TextBox();
+            this.rdMale = new System.Windows.Forms.RadioButton();
+            this.rdFemale = new System.Windows.Forms.RadioButton();
+            this.lbEmpName = new System.Windows.Forms.Label();
+            this.lbPhone = new System.Windows.Forms.Label();
+            this.lbGender = new System.Windows.Forms.Label();
+            this.lbDegree = new System.Windows.Forms.Label();
+            this.cboDegree = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.mtxtPhone = new System.Windows.Forms.MaskedTextBox();
+            this.gbGender = new System.Windows.Forms.GroupBox();
+            this.gbGender.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbTitle.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbTitle.Location = new System.Drawing.Point(233, 9);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(344, 59);
+            this.lbTitle.TabIndex = 0;
+            this.lbTitle.Text = "Employee Details";
+            // 
+            // lbEmpID
+            // 
+            this.lbEmpID.AutoSize = true;
+            this.lbEmpID.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbEmpID.Location = new System.Drawing.Point(189, 91);
+            this.lbEmpID.Name = "lbEmpID";
+            this.lbEmpID.Size = new System.Drawing.Size(122, 28);
+            this.lbEmpID.TabIndex = 1;
+            this.lbEmpID.Text = "Employee ID";
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.Location = new System.Drawing.Point(372, 95);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(239, 27);
+            this.txtEmployeeID.TabIndex = 2;
+            // 
+            // txtEmployeeName
+            // 
+            this.txtEmployeeName.Location = new System.Drawing.Point(372, 149);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.Size = new System.Drawing.Size(239, 27);
+            this.txtEmployeeName.TabIndex = 5;
+            // 
+            // rdMale
+            // 
+            this.rdMale.AutoSize = true;
+            this.rdMale.Checked = true;
+            this.rdMale.Location = new System.Drawing.Point(31, 18);
+            this.rdMale.Name = "rdMale";
+            this.rdMale.Size = new System.Drawing.Size(63, 24);
+            this.rdMale.TabIndex = 7;
+            this.rdMale.TabStop = true;
+            this.rdMale.Text = "Male";
+            this.rdMale.UseVisualStyleBackColor = true;
+            // 
+            // rdFemale
+            // 
+            this.rdFemale.AutoSize = true;
+            this.rdFemale.Location = new System.Drawing.Point(145, 18);
+            this.rdFemale.Name = "rdFemale";
+            this.rdFemale.Size = new System.Drawing.Size(78, 24);
+            this.rdFemale.TabIndex = 8;
+            this.rdFemale.Text = "Female";
+            this.rdFemale.UseVisualStyleBackColor = true;
+            // 
+            // lbEmpName
+            // 
+            this.lbEmpName.AutoSize = true;
+            this.lbEmpName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbEmpName.Location = new System.Drawing.Point(189, 145);
+            this.lbEmpName.Name = "lbEmpName";
+            this.lbEmpName.Size = new System.Drawing.Size(155, 28);
+            this.lbEmpName.TabIndex = 9;
+            this.lbEmpName.Text = "Employee Name";
+            // 
+            // lbPhone
+            // 
+            this.lbPhone.AutoSize = true;
+            this.lbPhone.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbPhone.Location = new System.Drawing.Point(189, 196);
+            this.lbPhone.Name = "lbPhone";
+            this.lbPhone.Size = new System.Drawing.Size(67, 28);
+            this.lbPhone.TabIndex = 10;
+            this.lbPhone.Text = "Phone";
+            // 
+            // lbGender
+            // 
+            this.lbGender.AutoSize = true;
+            this.lbGender.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbGender.Location = new System.Drawing.Point(189, 246);
+            this.lbGender.Name = "lbGender";
+            this.lbGender.Size = new System.Drawing.Size(76, 28);
+            this.lbGender.TabIndex = 11;
+            this.lbGender.Text = "Gender";
+            // 
+            // lbDegree
+            // 
+            this.lbDegree.AutoSize = true;
+            this.lbDegree.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbDegree.Location = new System.Drawing.Point(189, 293);
+            this.lbDegree.Name = "lbDegree";
+            this.lbDegree.Size = new System.Drawing.Size(75, 28);
+            this.lbDegree.TabIndex = 12;
+            this.lbDegree.Text = "Degree";
+            // 
+            // cboDegree
+            // 
+            this.cboDegree.FormattingEnabled = true;
+            this.cboDegree.Items.AddRange(new object[] {
+            "Ph. D. ",
+            "Master",
+            "Engineer",
+            "Master",
+            "Bachelor",
+            "Technician"});
+            this.cboDegree.Location = new System.Drawing.Point(372, 297);
+            this.cboDegree.Name = "cboDegree";
+            this.cboDegree.Size = new System.Drawing.Size(239, 28);
+            this.cboDegree.TabIndex = 13;
+            this.cboDegree.Text = "---Select Degree---";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSave.Location = new System.Drawing.Point(372, 356);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 29);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCancel.Location = new System.Drawing.Point(517, 356);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(94, 29);
+            this.btnCancel.TabIndex = 15;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // mtxtPhone
+            // 
+            this.mtxtPhone.Location = new System.Drawing.Point(372, 200);
+            this.mtxtPhone.Mask = "000-0000000";
+            this.mtxtPhone.Name = "mtxtPhone";
+            this.mtxtPhone.Size = new System.Drawing.Size(239, 27);
+            this.mtxtPhone.TabIndex = 16;
+            // 
+            // gbGender
+            // 
+            this.gbGender.Controls.Add(this.rdFemale);
+            this.gbGender.Controls.Add(this.rdMale);
+            this.gbGender.Location = new System.Drawing.Point(372, 233);
+            this.gbGender.Name = "gbGender";
+            this.gbGender.Size = new System.Drawing.Size(239, 45);
+            this.gbGender.TabIndex = 17;
+            this.gbGender.TabStop = false;
+            // 
+            // frmEmployeeDetails
+            // 
+            this.AcceptButton = this.btnSave;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbGender);
+            this.Controls.Add(this.mtxtPhone);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.cboDegree);
+            this.Controls.Add(this.lbDegree);
+            this.Controls.Add(this.lbGender);
+            this.Controls.Add(this.lbPhone);
+            this.Controls.Add(this.lbEmpName);
+            this.Controls.Add(this.txtEmployeeName);
+            this.Controls.Add(this.txtEmployeeID);
+            this.Controls.Add(this.lbEmpID);
+            this.Controls.Add(this.lbTitle);
+            this.Name = "frmEmployeeDetails";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.gbGender.ResumeLayout(false);
+            this.gbGender.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.Label lbEmpID;
+        private System.Windows.Forms.TextBox txtEmployeeID;
+        private System.Windows.Forms.TextBox txtEmployeeName;
+        private System.Windows.Forms.RadioButton rdMale;
+        private System.Windows.Forms.RadioButton rdFemale;
+        private System.Windows.Forms.Label lbEmpName;
+        private System.Windows.Forms.Label lbPhone;
+        private System.Windows.Forms.Label lbGender;
+        private System.Windows.Forms.Label lbDegree;
+        private System.Windows.Forms.ComboBox cboDegree;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.MaskedTextBox mtxtPhone;
+        private System.Windows.Forms.GroupBox gbGender;
+    }
+}
