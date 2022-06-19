a5d46f30e18324922fa61ade1d40123abb3613cd
QuangPham27 <101493497+QuangPham27@users.noreply.github.com>
2022-06-19 23:32:44 +0700

    Add files via upload

---
+++ Ass01Solution/DataAccess/Enums/SearchBy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Enums
+{
+    public enum SearchBy
+    {   
+        ByRole = 5,
+        ByCountry = 4,
+        ByCity = 3,
+        ByEmail = 2,
+        ByName = 1,
+        ById = 0
+    }
+}
